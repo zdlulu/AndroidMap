0c269045acb09913052dfa0870ebc5bb1f6d79b2
zhaolu <zhaolu@jiuan.com>
2015-10-20 16:03:01 +0800

    可正确显示百度地图，只是显示地图的功能

--- gen/com/example/mymaptest/R.java
+++ gen/com/example/mymaptest/R.java
@@ -25,7 +25,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int id_bmapView=0x7f080000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -34,9 +34,7 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
